b9a2361629f96c2b7cb6c96a95569b0a4d3f2c16
ShuaibAlhajj <shuaibalhajj@gmail.com>
2025-05-25 22:28:47 +0300

    Add files via upload
    total class

---
+++ Total.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee_Shop
+{
+    class Total
+    {
+        private double s = 0;
+        
+        public void addSum(double s)
+        {
+            this.s += s;
+        }
+
+        public double getSum()
+        {
+            return s;
+        } 
+
+        public double getTax()
+        {
+            return Math.Round(s * (9.0 / 100.0),2);
+        }
+
+        public double sumTax()
+        {
+            return getTax()+s;
+        }
+
+    }
+}
